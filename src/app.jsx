9ebd3c4ff5f80756374ad19775d0cc89c5a689ee
deving7788 <frank77499@gmail.com>
2024-10-15 08:58:39 -0700

    20241015

--- src/app.jsx
+++ src/app.jsx
@@ -1,6 +1,6 @@
 import {useEffect} from "react";
 import {Router, setBaseURL} from "./router.jsx";
-import {Home, MainFrame, ErrPage, Blogs, Forum, SignupGate, UserPanelGate, LoginGate, Blog} from "./pages";
+import {Home, MainFrame, ErrPage, Blogs, SignupGate, UserPanelGate, LoginGate, Blog} from "./pages";
 import {useMainContext} from "./context/mainContext.jsx";
 import {authenticate} from "./auth";
 import {frontendHost} from "./assets/data.jsx";
@@ -25,7 +25,6 @@ function App() {
        {path: "/article", component: <Blog/>,},
        {path: "/signup", component: <SignupGate/>,},
        {path: "/login", component: <LoginGate/>,},
-       {path: "/forum", component: <Forum/>,},
        {path: "/user-panel", component: <UserPanelGate/>,},
      ],},
      {path: "/*", component: <ErrPage/>,},
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,10 +2,9 @@ import Home from "./home.jsx";
 import MainFrame from "./mainFrame.jsx";
 import ErrPage from "./errPage.jsx";
 import Blogs from "./blogs.jsx";
-import Forum from "./forum.jsx";
 import SignupGate from "./signupGate.jsx";
 import UserPanelGate from "./userPanelGate.jsx";
 import LoginGate from "./loginGate.jsx";
 import Blog from "./blog.jsx";
 
-export {Home, MainFrame, ErrPage, Blogs, Forum, SignupGate, UserPanelGate, LoginGate, Blog};
+export {Home, MainFrame, ErrPage, Blogs, SignupGate, UserPanelGate, LoginGate, Blog};
